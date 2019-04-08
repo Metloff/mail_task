415d325eccb9c01f468098a2b81deba82de04de6
Henry Metlov <genrih.metlov@gmail.com>
2019-04-08 12:40:59 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	transport = &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout: 10 * time.Second,
+		}).DialContext,
+	}
+
+	client = &http.Client{
+		Timeout:   15 * time.Second,
+		Transport: transport,
+	}
+
+	workerLimit = 2
+)
+
+type result struct {
+	index  int
+	count  int
+	source string
+}
+
+func main() {
+	// Канал, ограничивающий количество одновременно запущенных горутин.
+	qoutaC := make(chan struct{}, workerLimit)
+	// Канал с результатами подсчетов.
+	resultsC := make(chan *result, 1)
+
+	wgResult := &sync.WaitGroup{}
+	wgWorkers := &sync.WaitGroup{}
+
+	// Запускаем в отдельной рутине функцию, для вывода результатов работы.
+	go printResult(resultsC, wgResult)
+	wgResult.Add(1)
+
+	// Считываем Stdin.
+	in := bufio.NewScanner(os.Stdin)
+	for i := 0; in.Scan(); i++ {
+		source := in.Text()
+		var content io.ReadCloser
+		var err error
+
+		if isURL(source) {
+			content, err = getContentFromSite(source)
+		} else {
+			content, err = getContentFromFile(source)
+		}
+		if err != nil {
+			continue
+		}
+
+		// Пробуем положить пустую структуру в канал.
+		qoutaC <- struct{}{}
+		wgWorkers.Add(1)
+
+		r := &result{
+			index:  i,
+			source: source,
+		}
+		go wordCounter(content, r, resultsC, qoutaC, wgWorkers)
+	}
+
+	wgWorkers.Wait()
+
+	close(qoutaC)
+	close(resultsC)
+
+	wgResult.Wait()
+}
+
+// printResult - вычитывает из канала и накапливает результаты работы подсчета.
+// Сортирует результаты в порядке подачи источников.
+// По окончанию работы программы выводит результаты и суммарное количество найденных совпадений.
+func printResult(resultsC chan *result, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var results []result
+	var total int
+
+	for val := range resultsC {
+		results = append(results, *val)
+		total += val.count
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].index < results[j].index
+	})
+
+	for _, result := range results {
+		log.Println(fmt.Sprintf("Count for %s: %v", result.source, result.count))
+	}
+
+	log.Println("Total:", total)
+}
+
+// getContentFromFile - получает контент из файла.
+func getContentFromFile(path string) (io.ReadCloser, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Println(fmt.Sprintf("Read file error: %v", err))
+		return nil, err
+	}
+
+	return file, nil
+}
+
+// getContentFromSite - получает контент с сайта.
+func getContentFromSite(url string) (io.ReadCloser, error) {
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Println(fmt.Sprintf("Get request error: %v", err))
+		return nil, err
+	}
+
+	return resp.Body, nil
+}
+
+// wordCounter - считает количество вхождений строки в контенте.
+func wordCounter(content io.ReadCloser, r *result, resultsC chan *result, qoutaC chan struct{}, wg *sync.WaitGroup) {
+	defer content.Close()
+	defer wg.Done()
+
+	scanner := bufio.NewScanner(content)
+	for i := 0; scanner.Scan(); i++ {
+		r.count += strings.Count(scanner.Text(), "Go")
+	}
+
+	resultsC <- r
+	<-qoutaC
+}
+
+// isURL - проверяет, является ли источник урлом.
+func isURL(source string) bool {
+	url, err := url.ParseRequestURI(source)
+	if err != nil {
+		return false
+	}
+	if len(url.Scheme) == 0 {
+		return false
+	}
+
+	return true
+}
+
+// echo -e 'https://golang.org\n/etc/passwd\nhttps://golang.org\nhttps://golang.org' | go run main.go
+// echo -e 'https://golang.org\n/etc/passwd\nhttps://golang.org\nhttps://golang.org' | go run main.go -cpuprofile=cpu.out -memprofile=mem.out
